c18027f745c8b9714e2065dec88cdd27b6578e6d
Piradharsan Navaratnam <Piradharsan.Navaratnam@effision.com>
2018-02-01 01:10:28 +1100

    swagger

--- iplatform-api/src/main/java/aon/iplatform/api/InsuranceController.java
+++ iplatform-api/src/main/java/aon/iplatform/api/InsuranceController.java
@@ -3,6 +3,10 @@ package aon.iplatform.api;
 import aon.iplatform.domain.model.common.Customer;
 import aon.iplatform.domain.model.common.Quote;
 import aon.iplatform.service.business.InsuranceService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +23,16 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/v1")
+@Api(value = "Insurance API", description = "Exposes APIs for insurance platform")
 public class InsuranceController {
 
     @Autowired
     private InsuranceService insuranceService;
 
+    @ApiOperation(value = "Gets quotes", response = Quote.class, responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Quote.class, responseContainer = "List")
+    })
     @RequestMapping(path = "/customer/quotes", method = RequestMethod.POST)
     public ResponseEntity<List<Quote>> greeting(@Valid @RequestBody Customer customer) {
         return ResponseEntity.status(HttpStatus.OK).body(insuranceService.getQuotes(customer));
